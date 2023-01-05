4553bc7189fbc528195d604b1430e22384708d05
Links <963030205@qq.com>
2023-01-06 00:27:29 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ja-homepage/'
+  base: 'https://linksray.github.io/ja-homepage/'
 })
